124668c8836409e5e0259f15b4c9e36bd7b49e98
beowulf <61982523+beowolx@users.noreply.github.com>
2025-02-17 09:36:28 +0100

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+
+from crew import StockAnalysisCrew
+
+
+def run():
+  company_stock = input(
+    "Enter the stock symbol to analyze (e.g., AAPL, MSFT): "
+  ).upper()
+  inputs = {
+    "query": "What is the company you want to analyze?",
+    "company_stock": company_stock,
+  }
+  return StockAnalysisCrew(inputs=inputs).crew().kickoff()
+
+
+def train():
+  """
+  Train the crew for a given number of iterations.
+  """
+  inputs = {
+    "query": "What is last years revenue",
+    "company_stock": "AMZN",
+  }
+  try:
+    StockAnalysisCrew().crew().train(
+      n_iterations=int(sys.argv[1]), inputs=inputs
+    )  # type: ignore
+
+  except Exception as e:
+    raise Exception(f"An error occurred while training the crew: {e}")
+
+
+if __name__ == "__main__":
+  print("## Welcome to Stock Analysis Crew")
+  print("-------------------------------")
+  result = run()
+  print("\n\n########################")
+  print("## Here is the Report")
+  print("########################\n")
+  print(result)
